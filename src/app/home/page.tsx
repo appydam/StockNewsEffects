f583525674f8df3288b2700e26d3ada46246cc1d
Arpit Dhamija <arpitdhamija@Arpits-MacBook-Air.local>
2024-11-13 02:31:17 +0530

    added authentication

---
+++ src/app/home/page.tsx
@@ -0,0 +1,31 @@
+
+import { RedirectToSignIn, SignedIn, SignedOut } from '@clerk/nextjs';
+import { currentUser } from '@clerk/nextjs/server'
+import React from 'react'
+
+type Props = {}
+
+const page = async (props: Props) => {
+
+    const user = await currentUser();
+
+
+    return (
+        <div>
+            {user?.firstName}
+            <div>
+                <SignedIn>
+                    <div>
+                        <h2>Protected Component A</h2>
+                        <p>Welcome to the protected component! Only authenticated users can see this.</p>
+                    </div>
+                </SignedIn>
+                <SignedOut>
+                    <h2>kuch nhi dikhega. Pehle sign in kar</h2>
+                </SignedOut>
+            </div>
+        </div>
+    )
+}
+
+export default page;
